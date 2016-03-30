ac439cf687ffb4f3fb3c79677cc4785e6b3aa59d
Roma Myakinin <omfis68@hotmail.com>
2016-03-30 23:43:06 +0300

    Переделал api response, теперь от внутри container.onclik

--- custom.js
+++ custom.js
@@ -22,41 +22,51 @@
 		container.style.backgroundSize = "27px 27px";
 		container.style.width = '27px';
 		container.style.height = '27px';
-
-		//1. Объект XMLHttpRequest (запрос к API)
-		var xhr = new XMLHttpRequest();
-
-		//2. Конфигурируем его: GET-запрос на openweathermap
-		xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=Tambov&lang=ru&units=metric&appid=4db4c7a9ac09694c25eeac0b990bba8b', true);
-	
-		//3. Отсылаем запрос
-		xhr.send();
+		
 
 		container.onclick = function(){
-
-		var otvet = xhr.responseText;
-
-		/*name,coord.lat,coord.lon,weather.description,main.temp,wind.speed
-		выше имена используемых параметров. Параметр weather.description не желает использоваться как
-		собственно и все остальные подпараметры weather. Вывел их в комментарии, т.к. описание в мане уже не актуально*/
-
+		//Создаем функцию для подключения к апи, и вызываем ее из container.onclick
+		function createConn(){
+			
+			var xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function(){
+				if(xhr.readyState == 4 && xhr.status == 200){
+					myFunction(xhr);
+				}
+			};
+			xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=Tambov&lang=ru&units=metric&appid=4db4c7a9ac09694c25eeac0b990bba8b', true);
+			xhr.send();
+		}
+		function myFunction(xml){
+		var otvet = xml.responseText;
 		otvet = JSON.parse(otvet);
-
 		L.marker(map.getCenter())
 		.addTo(map)
 		.bindPopup('<p></p><img src=\"images/owmloading.gif\" width=\"110\" height=\"110\">')
 		.openPopup().update();
-		
+		//поменять на JSON
 		function func(){	
 		L.marker(map.getCenter())
 		.addTo(map)
 		.bindPopup("<strong><h1>" + otvet.name + "</h1></strong><br>"
 		+ "<b>Координаты: </b>" + otvet.coord.lon + " / " + otvet.coord.lat + "<br>"
-		+ "<b>Описание: </b>" + otvet.weather.description + "<br>" + "<b>Температура: </b>"
+		+ "<b>Описание: </b>" + otvet.weather[0].description + "<br>" + "<b>Температура: </b>"
 		+ otvet.main.temp + "<br>" + "<b>Сила ветра: </b>" + otvet.wind.speed)
 		.openPopup().update();
 		}
 		setTimeout(func,3000);
+		}
+		createConn();
+		
+		
+		
+
+		/*name,coord.lat,coord.lon,weather.description,main.temp,wind.speed
+		выше имена используемых параметров. Параметр weather.description не желает использоваться как
+		собственно и все остальные подпараметры weather. Вывел их в комментарии, т.к. описание в мане уже не актуально*/
+
+
+		
 		
 		}
 		
